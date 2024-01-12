40bc555d014874564f739e96ebb8fd58e4c6bc06
Kuwar20 <kuwarx1@gmail.com>
2024-01-12 23:53:10 +0530

    Refactor HomeScreen component and add state management

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useStore } from '../store/store';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+
+const getCategoriesFormData = (data: any) => {
+    let temp: any = {};
+    for (let i = 0; i < data.length; i++) {
+        if (temp[data[i].name] === undefined) {
+            temp[data[i].name] = 1;
+        } else {
+            temp[data[i].name]++;
+        }
+    }
+    let categories = Object.keys(temp);
+    categories.unshift('All');
+    return categories;
+};
+const getCoffeeList = (category: string, data: any) => {
+    if (category === 'All') {
+        return data;
+    } else {
+        let coffeelist = data.filter((item: any) => item.name === category);
+        return coffeelist;
+    }
+};
 
 const HomeScreen = () => {
+    const CoffeeList = useStore((state: any) => state.CoffeeList);
+    const BeanList = useStore((state: any) => state.BeanList);
+    const [categories, setCategories] = useState(
+        getCategoriesFormData(CoffeeList),
+    );
+    const [searchText, setSearchText] = useState(undefined);
+    const [categoryIndex, setCategoryIndex] = useState({
+        index: 0,
+        category: categories[0],
+    });
+    const [sortedCoffee, setSortedCoffee] = useState(
+        getCoffeeList(categoryIndex.category, CoffeeList),
+    );
+
+    const tabBarHeight = useBottomTabBarHeight();
+    
     return (
         <View>
             <Text>HomeScreen</Text>
         </View>
     );
 };
-
 
 const styles = StyleSheet.create({});
 
